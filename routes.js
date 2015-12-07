5d46245527244091593ebc720b91b829384870a0
Ekuklu, B <bugraekuklu@hotmail.de>
2015-12-08 01:17:01 +0200

    added 3 letter code search support

--- routes.js
+++ routes.js
@@ -9,27 +9,14 @@
     'use strict'
 
     module.exports.start = function (port) {
-        var Sync = require('./mongodb/sync')
+        var Sync        = require('./mongodb/sync')
+        var Parser      = require('./mongodb/parser')
 
-        var Hapi = require('hapi')
+        var Hapi        = require('hapi')
 
         var server = new Hapi.Server()
 
         server.connection({ port: port ? port:3000 })
-
-        function courseWithCRN(request, reply) {
-            Sync.get(request.params.string, function (err, results) {
-                if (!err) {
-                    if (results) {
-                        reply(results)
-                    } else {
-                        reply({ error: 'RawSection with crn \'' + request.params.crn + '\' not found'})
-                    }
-                } else {
-                    reply(err)
-                }
-            })
-        }
 
         server.route({
             method: 'GET',
@@ -38,7 +25,7 @@
                 Sync.get(parseInt(request.params.crn), function (err, results) {
                     if (!err) {
                         if (results.length) {
-                            reply(results)
+                            reply(results[0])
                         } else {
                             reply({
                                 Error: 'RawSection with course reference number (CRN) \'' + request.params.crn + '\' could not be found.',
@@ -60,37 +47,44 @@
                 var isArgumentsValid = (request.query.string !== undefined || request.query.identifier !== undefined) && !(request.query.string !== undefined && request.query.identifier !== undefined)
 
                 if (isArgumentsValid && request.query.string !== undefined) {
-                    Sync.resolveString(request.query.string, function (err, results) {
-                        if (!err) {
-                            if (results.length) {
-                                reply(results)
-                            } else {
-                                reply({
-                                    Error: 'RawSection with string \'' + request.query.string + '\' could not be found.',
-                                    Description: null,
-                                })
-                            }
-                        } else {
-                            console.error(err)
-                            reply(err)
+                    Sync.db.collection('RawSections').createIndex({ title: 'text', instructor: 'text' }, function (err, result) {
+                        if (err) reply(err)
+                        else {
+                            Sync.db.collection('RawSections').find({ $text: { $search: request.query.string } }).toArray(function (err, result) {
+                                if (err) reply(err)
+                                else if (result.length) {
+                                    reply(result)
+                                } else {
+                                    reply({
+                                        Error: 'RawSection with string \'' + request.query.string + '\' could not be found.',
+                                        Description: null,
+                                    })
+                                }
+                            })
                         }
                     })
                 } else if (isArgumentsValid && request.query.identifier !== undefined) {
-                    Sync.resolveIdentifier(request.query.identifier, function (err, results) {
-                        if (!err) {
-                            if (results.length) {
-                                reply(results)
-                            } else {
-                                reply({
-                                    Error: 'RawSection with identifier \'' + request.query.identifier + '\' could not be found.',
-                                    Description: null,
-                                })
-                            }
+                    function callback(err, results) {
+                        if (err) reply(err)
+                        else if (results.length) {
+                            reply(results)
                         } else {
-                            console.error(err)
-                            reply(err)
+                            reply({
+                                Error: 'RawSection with identifier \'' + request.query.identifier + '\' could not be found.',
+                                Description: null,
+                            })
                         }
-                    })
+                    }
+
+                    if (request.query.identifier.length > 3) {
+                        var courseObject = Parser.parseIdentifier(request.query.identifier)
+
+                        Sync.db.collection('RawSections').find({ code: courseObject.code, number: courseObject.number, isEnglish: courseObject.isEnglish }).toArray(callback)
+                    } else if (string.length == 3) {
+                        Sync.db.collection('RawSections').find({ code: request.query.identifier }).toArray(callback)
+                    } else {
+                        callback(Error('At least 3 characters needed to do identifier search.'))
+                    }
                 } else {
                     reply({
                         Error: 'Invalid API parameters',
