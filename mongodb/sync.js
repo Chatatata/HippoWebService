21a4ccc9b2ce38c725583ebe7bc71cd667a51cc5
Ekuklu, B <bugraekuklu@hotmail.de>
2015-12-08 10:01:18 +0200

    added node shebangs and license disclaimers

--- mongodb/sync.js
+++ mongodb/sync.js
@@ -1,12 +1,34 @@
-//  sync.js
+#!/usr/bin/env node
+
+//    Copyright (c) 2015 The Digital Warehouse
 //
-//  Written by Buğra Ekuklu, The Digital Warehouse. Copyright 2015.
-//  thedigitalwarehouse.com
 //
-//    @description: Root server synchronization handler, based on Amazon Web Services DynamoDB Javascript low-level API
-//                  This module does not depend on an additional database manager module, unlike SQL version.
 //
-//      DynamoDB, raw.
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+
+//    routes.js
+//
+//    @description: Root server synchronization handler, based on MongoDB.
 
 (function () {
     'use strict'
@@ -21,7 +43,7 @@
      ,  MongoClient = require('mongodb').MongoClient                 //  MongoDB driver
      ,  db          = null
 //     ,  Analytics   = require('../analytics')
-     ,  Parser      = require('./parser')
+     ,  Parser      = require('./scheduleParser')
 
     var buildings   = require('../static/Buildings');             //  Load static data
     var courseCodes = require('../static/CourseCodes');
--- portalParser.js
+++ portalParser.js
@@ -1,47 +1,72 @@
-//  enrollment.js
+#!/usr/bin/env node
+
+//    Copyright (c) 2015 The Digital Warehouse
 //
-//  Written by Buğra Ekuklu, The Digital Warehouse. Copyright 2015.
-//  thedigitalwarehouse.com
 //
-//    @description: Course enrollments
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+
+//    enrollment.js
+//
+//    @description: Hippo parser module
 
 (function() {
     'use strict'
-    
+
     var Q = require('q');               //  kriskowal's promise implementation
-    
+
     var nodefiedrequest = require('request');
     var request = function (options) {
         var deferred = Q.defer();
-        
+
         nodefiedrequest(options, function (error, response, body) {
             if (error) deferred.reject(error);
             else {
                 var Response = {};
-                
+
                 Response.response = response;
                 Response.body = body;
-                
+
                 deferred.resolve(Response);
             }
         });
-        
+
         return deferred.promise;
     }
-    
+
     var DomParser = require('dom-parser');
-    
+
     var debug = true;
-        
+
 //        getAccountInformation();
-    
+
     function getAccountInformation(account) {
         var results = [];
-        
+
         var cookieJar = nodefiedrequest.jar();
-        
+
         return Q.resolve()
-        
+
         //  0
         .then(function () {
             var options = {
@@ -59,17 +84,17 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
-        
+
         //  1
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Could not connect to server.'));
-            
+
             var options = {
                 method: 'GET',
                 url: result.response.headers.location,
@@ -85,20 +110,20 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  2
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             var options = {
                 method: 'GET',
                 url: 'https://giris.itu.edu.tr' + result.response.headers.location,
@@ -114,28 +139,28 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  3
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 200) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             //  Get current .NET viewstate
-            
+
             var parser = new DomParser();
-            
+
             var dom = parser.parserFromString(result.body);
-            
-            
-            
+
+
+
             var options = {
                 method: 'POST',
                 url: 'https://giris.itu.edu.tr' + results[1].response.headers.location,
@@ -163,20 +188,20 @@
                     'LoginBtn.y': '14',
                 },
             }
-            
+
 //            return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  4
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             var options = {
                 method: 'GET',
                 url: 'https://giris.itu.edu.tr' + result.response.headers.location,
@@ -194,20 +219,20 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  5
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             var options = {
                 method: 'GET',
                 url: 'https://giris.itu.edu.tr' + result.response.headers.location,
@@ -225,20 +250,20 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  6
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             var options = {
                 method: 'GET',
                 url: result.response.headers.location,
@@ -256,20 +281,20 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  7   ssb.sis.itu.edu.tr:9000
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
             var options = {
                 method: 'GET',
                 url: result.response.headers.location,
@@ -287,20 +312,20 @@
                 followRedirect: false,
                 jar: cookieJar,
             }
-            
+
             return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
         //  8   ssb.sis.itu.edu.tr:9000
         .then(function (result) {
             console.log(result.response.statusCode);
             results.push(result);
-            
+
             if (result.response.statusCode != 302) return Q.reject(Error('Couldn't connect to server.1'));
-            
+
 //            var options = {
 //                method: 'POST',
 //                url: 'http://ssb.sis.itu.edu.tr:9000/pls/PROD/twbkwbis.P_ValLogin',
@@ -324,15 +349,15 @@
 //            SessionId:
 //            }
 //            }
-//            
+//
 //            return request(options);
         })
         .catch(function (error) {
             console.error(error);
         })
-        
+
     }
-    
+
     function debugLog(string) {
         if (debug) {
             return console.log(string);
