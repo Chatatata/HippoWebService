2a7ee2947de549f51d6617a03b9d2fdaa112016e
Ekuklu, B <bugraekuklu@hotmail.de>
2015-12-24 14:46:22 +0200

    added config file

--- Application.js
+++ Application.js
@@ -26,7 +26,7 @@
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //    THE SOFTWARE.
 
-//    main.js
+//    Application.js
 //
 //    ECMAScript 6, conforms to ECMA 2016 Javascript Standard.
 //
@@ -37,130 +37,128 @@
 //
 //    'use strict'; over the project due to ES6-dependent architecture!
 
-'use strict'                                            //  ES6
+(function () {
+    'use strict'
 
-var packageInfo     = require('./package.json')         //  Package info
+    var packageInfo     = require('./package.json')         //  Package file
+    var config          = require('./config.json')          //  Config file
 
-var VERSION         = packageInfo.version
+    console.log('hippohttpd (v. ' + packageInfo.version + ')\nType \'?\' to see help, \'.exit\' to exit.\n')
 
-console.log('hippohttpd (v. ' + VERSION + ')\nType \'?\' to see help, \'.exit\' to exit.\n')
+    var argv            = require('yargs').argv             //	Run argument vector parser
 
-var moment          = require('moment')                 //	Timing classes, moment.js
-var now             = require('performance-now')        //	Benchmarking, performance measuring
-var table           = require('text-table')             //	Console commands listing helper
-var assert          = require('assert')                 //	C type assertion test
-var argv            = require('yargs').argv             //	Run argument vector parser
+    //  Model classes
+     ,  User                = require('./models/User')
+     ,  Credentials         = require('./models/Credentials')
+     ,  Section             = require('./models/Section')
 
-//  Model classes
- ,  User                = require('./models/User')
- ,  Credentials         = require('./models/Credentials')
- ,  Section             = require('./models/Section')
+    var Util            = require('./Utility')
 
-var Util            = require('./Utility');             //  Utilities
+    var FetchManager    = require('./FetchManager')
 
-var FetchManager    = require('./FetchManager')
-if (!argv.test) FetchManager.init(argv.db)
+    module.exports = function () {
+        if (!argv.test) FetchManager.init(config.mongodb.url)
 
-//  Debug trigger
-var debug = true;
-if (!argv.port) console.log('No port specified, defaults to 3000.'.verbose)
-require('./RouteController').start(argv.port)               //  Main routes controller
+        require('./RouteController').start(config.hapi.port)               //  Main routes controller
 
-var readline = require('readline');
+        var readline = require('readline')
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
 
-rl.on('line', function (chunk) {
-    if (chunk !== null) {
-        var argv = chunk.split(' ')
+        rl.on('line', function (chunk) {
+            if (chunk !== null) {
+                var argv = chunk.split(' ')
 
-        argv.forEach(function (element) { element.trim() })
+                argv.forEach(function (element) { element.trim() })
 
-        switch (argv[0]) {
-            case 'db.deleteAll':
-                FetchManager.deleteAll(function (err) {
-                    if (err) console.error(err)
-                    else Util.log('Successfully destroyed.')
-                })
-                break
+                switch (argv[0]) {
+                    case 'db.deleteAll':
+                        FetchManager.deleteAll(function (err) {
+                            if (err) console.error(err)
+                            else Util.log('Successfully destroyed.')
+                        })
+                        break
 
-            case 'db.list':
-                FetchManager.countCollections(function (err, results) {
-                    if (err) console.error(err)
-                    else console.log(results)
-                })
-                break
+                    case 'db.list':
+                        FetchManager.countCollections(function (err, results) {
+                            if (err) console.error(err)
+                            else console.log(results)
+                        })
+                        break
 
-            case 'schedule.update':
-                FetchManager.updateSchedule(argv[1], function (err, result) {
-                    if (err) console.error(err)
-                    else Util.log('Successfully updated.')
-                })
-                break
+                    case 'schedule.update':
+                        FetchManager.updateSchedule(argv[1], function (err, result) {
+                            if (err) console.error(err)
+                            else Util.log('Successfully updated.')
+                        })
+                        break
 
-            case 'section.get':
-                Section.find(JSON.parse(argv[1]), function (err, section) {
-                    if (err) console.error(err)
-                    else Util.log(section)
-                })
-                break
+                    case 'section.get':
+                        Section.find(JSON.parse(argv[1]), function (err, section) {
+                            if (err) console.error(err)
+                            else Util.log(section)
+                        })
+                        break
 
-            case 'user.register':
-                if (argv.length == 4) {
-                    var credentials = {
-                        username: argv[1],
-                        password: argv[2],
-                        PIN: argv[3],
-                    }
+                    case 'user.register':
+                        if (argv.length == 4) {
+                            var credentials = {
+                                username: argv[1],
+                                password: argv[2],
+                                PIN: argv[3],
+                            }
 
-                    Util.log(' Checking account information')
-                    FetchManager.challenge(credentials, function (err) {
-                        if (err) console.error(err)
-                        else Util.log('Successfully challenged credential.')
-                    })
-                } else {
-                    Util.log(' Register Account')
-                    Util.log(' Usage: [COMMAND] [username] [password] [PIN]')
+                            Util.log(' Checking account information')
+                            FetchManager.challenge(credentials, function (err) {
+                                if (err) console.error(err)
+                                else Util.log('Successfully challenged credential.')
+                            })
+                        } else {
+                            Util.log(' Register Account')
+                            Util.log(' Usage: [COMMAND] [username] [password] [PIN]')
+                        }
+                        break
+
+                    case 'user.get':
+                        if (argv.length == 2) {
+                            Users.find({ id: argv[1] }).exec(function (err, user) {
+                                if (err) console.error(err)
+                                else console.log(user)
+                            })
+                        }
+
+                    case 'test.fetch':
+                        FetchManager.test.fetch(function (err, result, time) {
+                            if (err) console.error(err)
+                            else console.log(result, time)
+                        })
+                        break
+
+                    case 'util.debug':
+                        config.debug = !config.debug
+                        break
+
+                    case '?':
+                        Util.flushConsole(function () {
+                            Util.help()
+                        })
+                        break
+
+                    case '.exit':
+                        process.exit()
+                        break
+
+                    case '':
+                        break
+
+                    default:
+                        console.log('Unrecognized command: ' + argv[0])
                 }
-                break
-
-            case 'user.get':
-                if (argv.length == 2) {
-                    Users.find({ id: argv[1] }).exec(function (err, user) {
-                        if (err) console.error(err)
-                        else console.log(user)
-                    })
-                }
-
-            case 'test.fetch':
-                FetchManager.test.fetch(function (err, result, time) {
-                    if (err) console.error(err)
-                    else console.log(result, time)
-                })
-                break
-
-            case 'util.debug':
-                debug = !debug
-                break
-
-            case '?':
-                Util.flushConsole(function () {
-                    Util.help()
-                })
-                break
-
-            case '.exit':
-                process.exit()
-                break
-
-            case '':
-                break
-
-            default:
-                console.log('Unrecognized command: ' + argv[0])
-        }
+            }
+        })
     }
-})
+}())
+
